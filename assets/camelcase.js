c26ee07dd0afcaf8021cf05e598dd4801c6533de
Kirill <seokirillstarsnab@gmail.com>
2016-05-26 00:03:09 +0300

    optimize camelCase + snake_case

--- assets/camelcase.js
+++ assets/camelcase.js
@@ -1,18 +1,6 @@
 String.prototype.toCamelCase = function(){
-    return this.split('').map(function (char, i, word) {
-        if(char == '-'){
-            word[i + 1] = word[i + 1].toUpperCase();
-            return '';
-        }
-        return char;
-    }).join('');
-}
-String.prototype.deCamelCase = function(separator){
-    var separator = separator || '-';
-    return this.split('').map(function (char, i) {
-        if(char == char.toUpperCase()){
-            return i > 0 ? separator + char.toLowerCase() : char.toLowerCase();
-        }
-        return char;
-    }).join('');
+    return this.toLowerCase()
+        .replace( /\W+/g, ' ' )
+        .replace( / (.)/g, function($1) { return $1.toUpperCase(); })
+        .replace( / /g, '' );
 }

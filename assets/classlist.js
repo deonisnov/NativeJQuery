b2fd332637ea97b1a38fe2d1cdb8a29532ab4540
Kirill <seokirillstarsnab@gmail.com>
2016-05-26 19:37:00 +0300

    fix

--- assets/classlist.js
+++ assets/classlist.js
@@ -1,13 +1,11 @@
-NodeList.prototype.addClass = function(className){
-	this.forEach(function (elem) {
-		elem.addClass(className);
-	})
+Node.prototype.addClass = function(className){
+	this.classList.add(className);
 	return this;
 }
 
 NodeList.prototype.addClass = function(className){
-this.forEach(function (elem) {
-elem.addClass(className);
-})
-return this;
+	this.forEach(function (elem) {
+		elem.addClass(className);
+	});
+	return this;
 }

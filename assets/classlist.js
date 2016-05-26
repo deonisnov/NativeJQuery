66057177774d55731bfd7d565c6dd4ef6bdea694
Vladislav Zhurakosvkiy <dkfl123@bigmir.net>
2016-05-26 13:44:37 +0300

    NodeList

--- assets/classlist.js
+++ assets/classlist.js
@@ -2,3 +2,10 @@ Node.prototype.addClass = function(className){
 	this.classList.add(className);
 	return this;
 }
+
+NodeList.prototype.addClass = function(className){
+this.forEach(function (elem) {
+elem.addClass(className);
+})
+return this;
+}

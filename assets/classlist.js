552bb778ead062f106ace4923483974f09ab7360
Kirill <seokirillstarsnab@gmail.com>
2016-05-28 17:20:09 +0300

    classList

--- assets/classlist.js
+++ assets/classlist.js
@@ -1,11 +1,53 @@
-Node.prototype.addClass = function(className){
-	this.classList.add(className);
+// addClass()
+Node.prototype.addClass = function(classlist){
+	var self = this;
+	classlist
+		.split(' ')
+		.forEach(function (className) {
+			self.classList.add(className);
+		});
 	return this;
-}
+};
 
-NodeList.prototype.addClass = function(className){
+NodeList.prototype.addClass = function(classList){
 	this.forEach(function (elem) {
-		elem.addClass(className);
+		elem.addClass(classList);
 	});
 	return this;
-}
+};
+
+// removeClass()
+Node.prototype.removeClass = function(classlist){
+	var self = this;
+	classlist
+		.split(' ')
+		.forEach(function (className) {
+			self.classList.remove(className);
+		});
+	return this;
+};
+
+NodeList.prototype.removeClass = function(classList){
+	this.forEach(function (elem) {
+		elem.removeClass(classList);
+	});
+	return this;
+};
+
+// toggleClass()
+Node.prototype.toggleClass = function(classlist){
+	var self = this;
+	classlist
+		.split(' ')
+		.forEach(function (className) {
+			self.classList.toggle(className);
+		});
+	return this;
+};
+
+NodeList.prototype.toggleClass = function(classList){
+	this.forEach(function (elem) {
+		elem.toggleClass(classList);
+	});
+	return this;
+};
